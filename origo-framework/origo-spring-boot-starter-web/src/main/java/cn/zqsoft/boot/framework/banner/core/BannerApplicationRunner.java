1e0661cde640ab15c4877946847ae7d401ece22b
Euan <perfecteuan@163.com>
2024-11-05 09:28:58 +0800

    add:验证码底图

--- origo-framework/origo-spring-boot-starter-web/src/main/java/cn/zqsoft/boot/framework/banner/core/BannerApplicationRunner.java
+++ origo-framework/origo-spring-boot-starter-web/src/main/java/cn/zqsoft/boot/framework/banner/core/BannerApplicationRunner.java
@@ -24,47 +24,46 @@ public class BannerApplicationRunner implements ApplicationRunner {
                             "项目启动成功！\n\t" +
                             "接口文档: \t{} \n\t" +
                             "开发文档: \t{} \n\t" +
-                            "视频教程: \t{} \n" +
+
                             "----------------------------------------------------------",
-                    "https://doc.iocoder.cn/api-doc/",
-                    "https://doc.iocoder.cn",
-                    "https://t.zsxq.com/02Yf6M7Qn");
+                    "https://doc.deveuan.top/api-doc/",
+                    "https://doc.deveuan.top");
 
             // 数据报表
             if (isNotPresent("cn.zqsoft.boot.module.report.framework.security.config.SecurityConfiguration")) {
-                System.out.println("[报表模块 origo-module-report - 已禁用][参考 https://doc.iocoder.cn/report/ 开启]");
+                System.out.println("[报表模块 origo-module-report - 已禁用][参考 https://doc.deveuan.top/report/ 开启]");
             }
             // 工作流
             if (isNotPresent("cn.zqsoft.boot.module.bpm.framework.flowable.config.BpmFlowableConfiguration")) {
-                System.out.println("[工作流模块 origo-module-bpm - 已禁用][参考 https://doc.iocoder.cn/bpm/ 开启]");
+                System.out.println("[工作流模块 origo-module-bpm - 已禁用][参考 https://doc.deveuan.top/bpm/ 开启]");
             }
             // 商城系统
             if (isNotPresent("cn.zqsoft.boot.module.trade.framework.web.config.TradeWebConfiguration")) {
-                System.out.println("[商城系统 origo-module-mall - 已禁用][参考 https://doc.iocoder.cn/mall/build/ 开启]");
+                System.out.println("[商城系统 origo-module-mall - 已禁用][参考 https://doc.deveuan.top/mall/build/ 开启]");
             }
             // ERP 系统
             if (isNotPresent("cn.zqsoft.boot.module.erp.framework.web.config.ErpWebConfiguration")) {
-                System.out.println("[ERP 系统 origo-module-erp - 已禁用][参考 https://doc.iocoder.cn/erp/build/ 开启]");
+                System.out.println("[ERP 系统 origo-module-erp - 已禁用][参考 https://doc.deveuan.top/erp/build/ 开启]");
             }
             // CRM 系统
             if (isNotPresent("cn.zqsoft.boot.module.crm.framework.web.config.CrmWebConfiguration")) {
-                System.out.println("[CRM 系统 origo-module-crm - 已禁用][参考 https://doc.iocoder.cn/crm/build/ 开启]");
+                System.out.println("[CRM 系统 origo-module-crm - 已禁用][参考 https://doc.deveuan.top/crm/build/ 开启]");
             }
             // 微信公众号
             if (isNotPresent("cn.zqsoft.boot.module.mp.framework.mp.config.MpConfiguration")) {
-                System.out.println("[微信公众号 origo-module-mp - 已禁用][参考 https://doc.iocoder.cn/mp/build/ 开启]");
+                System.out.println("[微信公众号 origo-module-mp - 已禁用][参考 https://doc.deveuan.top/mp/build/ 开启]");
             }
             // 支付平台
             if (isNotPresent("cn.zqsoft.boot.module.pay.framework.pay.config.PayConfiguration")) {
-                System.out.println("[支付系统 origo-module-pay - 已禁用][参考 https://doc.iocoder.cn/pay/build/ 开启]");
+                System.out.println("[支付系统 origo-module-pay - 已禁用][参考 https://doc.deveuan.top/pay/build/ 开启]");
             }
             // AI 大模型
             if (isNotPresent("cn.zqsoft.boot.module.ai.framework.web.config.AiWebConfiguration")) {
-                System.out.println("[AI 大模型 origo-module-ai - 已禁用][参考 https://doc.iocoder.cn/ai/build/ 开启]");
+                System.out.println("[AI 大模型 origo-module-ai - 已禁用][参考 https://doc.deveuan.top/ai/build/ 开启]");
             }
             // IOT 物联网
             if (isNotPresent("cn.zqsoft.boot.module.iot.framework.web.config.IotWebConfiguration")) {
-                System.out.println("[IOT 物联网 origo-module-iot - 已禁用][参考 https://doc.iocoder.cn/iot/build/ 开启]");
+                System.out.println("[IOT 物联网 origo-module-iot - 已禁用][参考 https://doc.deveuan.top/iot/build/ 开启]");
             }
         });
     }
