65e97a5111fda361ba4c3a392742dd4ce1a5333c
Euan <perfecteuan@163.com>
2024-12-15 13:45:52 +0800

    add:增加批量消费策略bean

--- origo-framework/origo-spring-boot-starter-mq/src/main/java/cn/zqsoft/boot/framework/mq/rabbitmq/config/OrigoRabbitMQAutoConfiguration.java
+++ origo-framework/origo-spring-boot-starter-mq/src/main/java/cn/zqsoft/boot/framework/mq/rabbitmq/config/OrigoRabbitMQAutoConfiguration.java
@@ -1,16 +1,27 @@
 package cn.zqsoft.boot.framework.mq.rabbitmq.config;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.TimeZone;
 
 /**
  * RabbitMQ 消息队列配置类
@@ -23,12 +34,19 @@ import org.springframework.context.annotation.Bean;
 public class OrigoRabbitMQAutoConfiguration {
 
     @Bean
-    public ObjectMapper objectMapper() {
+    public ObjectMapper origoRabbitobjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         // 注册 Java 8 时间模块
         objectMapper.registerModule(new JavaTimeModule());
-        // 禁用序列化时写入时间戳
+
+        // 禁用时间戳序列化
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 设置反序列化处理
+        objectMapper.enable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+
         return objectMapper;
     }
 
@@ -36,8 +54,8 @@ public class OrigoRabbitMQAutoConfiguration {
      * Jackson2JsonMessageConverter Bean：使用 jackson 序列化消息
      */
     @Bean
-    public MessageConverter createMessageConverter(ObjectMapper objectMapper) {
-        return new Jackson2JsonMessageConverter(objectMapper);
+    public MessageConverter createMessageConverter(ObjectMapper origoRabbitobjectMapper) {
+        return new Jackson2JsonMessageConverter(origoRabbitobjectMapper);
     }
 
     // 配置 RabbitTemplate，绑定消息转换器
@@ -46,6 +64,18 @@ public class OrigoRabbitMQAutoConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    // 批量消费
+    @Bean(name = "origoBatchContainerFactory")
+    public SimpleRabbitListenerContainerFactory origoBatchContainerFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer, ConnectionFactory connectionFactory, ObjectMapper origoRabbitobjectMapper) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setBatchListener(true);
+        Jackson2JsonMessageConverter messageConverter = new Jackson2JsonMessageConverter(origoRabbitobjectMapper);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 
 }
