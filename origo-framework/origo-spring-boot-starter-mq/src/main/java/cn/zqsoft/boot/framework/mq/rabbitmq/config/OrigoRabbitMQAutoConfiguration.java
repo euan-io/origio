38b14ffcdcd3bf6ac7dc03491cc2fa9dad755dde
Euan <perfecteuan@163.com>
2024-12-15 11:34:09 +0800

    fix:LocalDataTime无法序列化的问题

--- origo-framework/origo-spring-boot-starter-mq/src/main/java/cn/zqsoft/boot/framework/mq/rabbitmq/config/OrigoRabbitMQAutoConfiguration.java
+++ origo-framework/origo-spring-boot-starter-mq/src/main/java/cn/zqsoft/boot/framework/mq/rabbitmq/config/OrigoRabbitMQAutoConfiguration.java
@@ -1,5 +1,8 @@
 package cn.zqsoft.boot.framework.mq.rabbitmq.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -19,12 +22,22 @@ import org.springframework.context.annotation.Bean;
 @ConditionalOnClass(name = "org.springframework.amqp.rabbit.core.RabbitTemplate")
 public class OrigoRabbitMQAutoConfiguration {
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 注册 Java 8 时间模块
+        objectMapper.registerModule(new JavaTimeModule());
+        // 禁用序列化时写入时间戳
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
     /**
      * Jackson2JsonMessageConverter Bean：使用 jackson 序列化消息
      */
     @Bean
-    public MessageConverter createMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter createMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     // 配置 RabbitTemplate，绑定消息转换器
