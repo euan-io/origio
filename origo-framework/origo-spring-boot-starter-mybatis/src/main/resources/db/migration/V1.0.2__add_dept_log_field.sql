b6005f54d4e14171ea51daeb23dca87772b269d5
Euan <perfecteuan@163.com>
2024-12-22 13:10:39 +0800

    add:添加修改表机构sql

---
+++ origo-framework/origo-spring-boot-starter-mybatis/src/main/resources/db/migration/V1.0.2__add_dept_log_field.sql
@@ -0,0 +1,21 @@
+ALTER TABLE `system_dept` ADD COLUMN `code` bigint NOT NULL DEFAULT 0 COMMENT '部门编码';
+
+ALTER TABLE `system_operate_log`
+    MODIFY `trace_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '链路追踪编号',
+    MODIFY `module` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '模块标题',
+    MODIFY `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '操作名',
+    MODIFY `content` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '操作内容',
+    MODIFY `exts` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '拓展字段',
+    MODIFY `request_method` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '请求方法名',
+    MODIFY `request_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '请求地址',
+    MODIFY `user_ip` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户 IP',
+    MODIFY `user_agent` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '浏览器 UA',
+    MODIFY `java_method` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'Java 方法名',
+    MODIFY `java_method_args` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT 'Java 方法的参数',
+    MODIFY `result_code` int NOT NULL DEFAULT '0' COMMENT '结果码',
+    MODIFY `result_msg` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '结果提示',
+    MODIFY `result_data` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '结果数据',
+    MODIFY `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '创建者',
+    MODIFY `updater` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '更新者',
+
+    MODIFY `sub_type` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '操作名';
